2c4d50f50fac0dda21e2f06d184148b8face884d
orel <zaaharaniaurellia@gmail.com>
2025-11-24 12:04:06 +0700

    ogin.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 include 'fungsi.php';
-
+echo password_hash("satu",PASSWORD_DEFAULT);
 if (isLoggedIn()) {
     header("Location: index.php");
     exit();
@@ -48,3 +48,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 </body>
 </html>
+
